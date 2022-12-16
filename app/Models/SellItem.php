a93a3dbefc858dbf6295ea943be0778652146b8e
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2022-12-15 22:01:55 -0300

    modelagem tabela de vendas e itens de venda

---
+++ app/Models/SellItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SellItem extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+      "sell_id",
+      "product_id",
+      "quantity",
+      "discount"
+    ];
+
+    public function sell(){
+        return $this->belongsTo(Sell::class);
+    }
+
+    public function product(){
+        return $this->hasOne(Product::class);
+    }
+}
---
+++ database/migrations/2022_12_16_005753_create_sell_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sell_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("sell_id")->constrained();
+            $table->foreignId("product_id")->constrained();
+            $table->integer("quantity");
+            $table->float("discount");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sell_items');
+    }
+};
